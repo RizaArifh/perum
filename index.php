36822ee57248b3e9e366328fe16866b28d9a0d61
RizaArifh <50792856+RizaArifh@users.noreply.github.com>
2020-07-07 22:00:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rian Properti</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <style>
+
+.header {
+  /* The image used */
+  background-image: url("assets/img/iklan/bg.jpeg");
+
+  /* Full height */
+  height: 100vh;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.full{
+    width: 100wh;
+
+}
+.bawah{
+    background:#EEEAE7;
+}
+    </style>
+</head>
+<body>
+<!-- <div class="container-fluid"> -->
+    
+<div class="row full">
+    
+    
+  <!-- collapse -->
+    
+
+<!-- close collapse -->
+<!-- <img src="assets/img/iklan/bg.jpeg" alt=""> -->
+<div class="badan">
+    <div class="header">
+    <h1 class="display-1 text-black bg-black rounderd-lg">Rian Properti</h1> 
+    <h1 class="display-4 text-black bg-black rounderd-lg">Pilihan Tepat Investasi Rumah</h1> 
+    
+    <a href="https://api.whatsapp.com/send?phone=+6285728470241&text=Permisi mau menanyakan tentang perumahan">
+    <button class="btn btn-success">Untuk Detail Lebih Lanjut Bisa Kontak WA</button>
+    </a>
+    </div>
+    <div class="bawah">
+<div class="row">
+    <div class="col-12 " align="center">
+<h3 class="display-3" >Penawaran Perumahan</h3> 
+</div>
+</div>
+
+<div class="row">
+<div class="card col-md-6 col-sm-12">
+<div class="col-12 " align="center">
+<h3>Subsidi</h3>
+</div>
+<div class="row">
+<div class="card col">
+<h4>Griya Cendana Kroyo 1</h4>
+<img src="assets/img/iklan/brosur_griya_cendana1.jpeg" class="w-100" alt="">
+</div>
+</div>
+</div>
+
+<div class="card col-md-6 col-sm-12 ">
+<div class="col-12 " align="center">
+<h3>Komersil</h3>
+</div>
+<div class="row">
+<div class="card col">
+<h4>Griya Cendana Kroyo 2</h4>
+<img src="assets/img/iklan/brosur_griya_cendana2.jpeg" class="w-100" alt="">
+</div>
+</div>
+</div>
+
+
+</div><!-- bawah -->
+</div> <!-- row subs x com -->
+</div><!-- badan -->
+</div><!-- row -->
+</div> <!-- container -->
+
+
+</body>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+</html>
